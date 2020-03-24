92f7615a94f3963dd36253873914a66e915114a4
ArthurDOTmpeg <Arthur.a379@gmail.com>
2020-03-24 17:17:05 +0100

    Controller Artists + template + route

--- app/Artists.php
+++ app/Artists.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artists extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['firstname', 'lastname'];
+
+   /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'artists';
+
+   /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
